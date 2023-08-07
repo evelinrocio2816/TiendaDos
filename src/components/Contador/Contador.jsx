f18566ea2c3b44e806441f1e770eee00bc59fe0d
Evelin Romero <Evelinrocio2816@gmail.com>
2023-08-07 02:00:43 -0300

    cambios

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -18,13 +18,14 @@ const Contador = () => {
   return (
     <main className="btnContainer">
         <h4>Pelotita de Goma</h4>
-        <img src="https://media.istockphoto.com/id/146026034/es/foto/juguete-de-mascota.jpg?s=612x612&w=0&k=20&c=pM5IdTJgaaY8U6e6JtMJbnMCTtyyZTw2nCkTNCmta34=" alt="Pelota" />
+        <img className="imgProducto" src="https://media.istockphoto.com/id/146026034/es/foto/juguete-de-mascota.jpg?s=612x612&w=0&k=20&c=pM5IdTJgaaY8U6e6JtMJbnMCTtyyZTw2nCkTNCmta34=" alt="Pelota" />
         <div className="botones">
         <button className="btnContador" onClick={restar}>-</button>
-        <button className="btnContador"onClick={reset}>restart</button>
+        <button  className="btnContador"  onClick={reset}>↩</button>
         <button className="btnContador"onClick={sumar}>+</button>
        
-        <button className="btnContador" >Cantidad: {contador}</button> </div>
+        <h1 > = {contador}</h1>
+         </div>
     </main>
  
   )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import NavLinks from "../NavLinks/NavLinks"
 const Footer = () => {
   return (
    <footer>
-  <p> Evelinrocio2023 &copy;Copyright- 2023</p>
+  <p>  &copy;Copyright 2023 - EvelinRocio2816</p>
    <NavLinks/>
    <img width="336" height="135" src="https://www.mitiendademascotas.com/web/wp-content/uploads/2017/07/logo-pie.png" sizes="(max-width: 336px) 100vw, 336px"/>
    
